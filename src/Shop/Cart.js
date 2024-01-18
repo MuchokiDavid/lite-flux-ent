3ee7c8fee1876d7ad51ab5e40deb160c9e3fbb12
Samuel Muigai <ndungusam01@gmail.com>
2024-01-18 19:34:53 +0300

    Changed the background color of the shop now and continue shopping buttons

--- src/Shop/Cart.js
+++ src/Shop/Cart.js
@@ -8,7 +8,7 @@ const Cart = ({cartItems}) =>
         return (
             <>
                 <p>Cart is empty!</p>
-                <Link to="/shop" className="btn btn-info">Shop now</Link>
+                <Link to="/shop" className="btn btn-info" style={{backgroundColor: "#005477", color: "white"}}>Shop now</Link>
             </>
         )
     }
@@ -21,7 +21,7 @@ const Cart = ({cartItems}) =>
                     <li key={index}>Name: {item.name}- Quantity: {item.quantity}</li>
                 ))}
             </ul>
-            <Link to="/shop" className="btn btn-info">Continue shopping</Link>
+            <Link to="/shop" className="btn btn-info" style={{backgroundColor: "#005477", color: "white"}}>Continue shopping</Link>
         </>
     );
 }
